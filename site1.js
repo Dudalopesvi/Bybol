895d016fb1d1bd92086f1f9dfb0b10ce5b65e235
Dudalopesvi <lopesvieiraeduarda@gmail.com>
2025-10-15 21:46:00 -0300

    tentando fazer o hidden funcionar

--- site1.js
+++ site1.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 
-let login = document.getElementById("login")
+let login = document.getElementById("login");
 
 let senha = document.getElementById("senha");
 
@@ -46,99 +45,36 @@ function Salvar() {
 
 function mostrarareadeLogin() {
 
-    document.getElementById("login").classList.remove("hidden")
-    document.getElementById("Criarconta").classList.add("hidden")
-    document.getElementById("explorar").classList.add("hidden")
-    document.getElementById("ModoAnônimo").classList.add("hidden")
+    document.getElementById("login1").classList.remove("hidden")
+    document.getElementById("Criarconta1").classList.add("hidden")
+    document.getElementById("Explorar1").classList.add("hidden")
+    document.getElementById("ModoAnônimo1").classList.add("hidden")
 }
 
 function mostrarareadecriarconta() {
 
-    document.getElementById("Criarconta").classList.remove("hidden")
-    document.getElementById("logi").classList.add("hidden")
-    document.getElementById("explorar").classList.add("hidden")
-    document.getElementById("ModoAnônimo").classList.add("hidden")
+    document.getElementById("Criarconta1").classList.remove("hidden")
+    document.getElementById("login1").classList.add("hidden")
+    document.getElementById("Explorar1").classList.add("hidden")
+    document.getElementById("ModoAnônimo1").classList.add("hidden")
 }
 
 
 function mostrarareadeExplorar() {
 
-    document.getElementById("Explorar").classList.remove("hidden")
-    document.getElementById("Criarconta").classList.add("hidden")
-    document.getElementById("login").classList.add("hidden")
-    document.getElementById("ModoAnônimo").classList.add("hidden")
+    document.getElementById("Explorar1").classList.remove("hidden")
+    document.getElementById("Criarconta1").classList.add("hidden")
+    document.getElementById("login1").classList.add("hidden")
+    document.getElementById("ModoAnônimo1").classList.add("hidden")
 }
 
 
 
 function mostrarareadeModoAnônimo() {
 
-    document.getElementById("ModoAnônimo").classList.remove("hidden")
-    document.getElementById("Criarconta").classList.add("hidden")
-    document.getElementById("login").classList.add("hidden")
-    document.getElementById("ModoAnônimo").classList.add("hidden")
+    document.getElementById("ModoAnônimo1").classList.remove("hidden")
+    document.getElementById("Criarconta1").classList.add("hidden")
+    document.getElementById("login1").classList.add("hidden")
+    document.getElementById("Explorar1").classList.add("hidden")
 }
 
-=======
-
-let login = document.getElementById("login")
-
-let senha = document.getElementById("senha");
-
-function cadastrar() {
-    let usuario = { login: login.value, senha: senha.value }
-    usuarios.push(usuario)
-    alert("usuário cadastrado com sucesso!")
-    localStorage.setItem("usuarios", JSON.stringify(usuarios))
-
-}
-
-function acessar() {
-
-    for (let i = 0; i < usuarios.length; i++) {
-        if (usuarios[i].login == login.value && usuarios[i].senha == senha.value) {
-            alert("Bem vindo")
-        }
-
-    }
-}
-let usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]")
-
-mostrarareadeLogin()
-function mudarpraoutratela() {
-
-    document.getElementById("Criarconta").classList.add("hidden")
-    document.getElementById("login").classList.remove("hidden")
-
-}
-
-function Salvar() {
-
-    let usuario = document.getElementById("usuarios").value
-    let senha = document.getElementById("senha").value
-
-    let conta = {
-        usuario: usuario,
-        senha: senha
-    }
-
-    usuarios.push(usuario);
- 
-    alert("Cadastro realizado com sucesso!")
-    mostrarareadeLogin()
-}
-
-function mostrarareadeLogin() {
-
-    document.getElementById("Criarconta").classList.remove("hidden")
-    document.getElementById("login").classList.add("hidden")
-
-    let corpotabela = document.getElementById("corpo");
-    corpotabela.innerHTML = ""
-    for (let i = 0; i < usuarios.length; i++) {
-        const linha = document.createElement("tr")
-
-    }
-}
-
->>>>>>> 5bded44265e80ba36fd231741f31918af424068d
